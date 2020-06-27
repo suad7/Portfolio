251cc1073e2ec6013162061de2a9a9edc69b8520
suad <suadshire98@gmail.com>
2020-06-28 02:00:28 +0300

    updated index.html

---
+++ js/suu.js
@@ -0,0 +1,9 @@
+
+  // var typed = new Typed('.moringa', {
+  //   strings: ["SUAD SHIRE",
+  //   "Software Developer",
+  //   'In Web Applications'],
+  //   typeSpeed:300,
+  //   backSpeed:300,
+  //   loop:true
+  // });
